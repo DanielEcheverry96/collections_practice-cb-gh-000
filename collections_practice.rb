a63fcd6dcce9e7e5790f9dbf832fafa8a46dfd0c
Daniel Echeverry Giraldo <dfecheverryg@unal.edu.co>
2018-02-11 01:53:00 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -48,5 +48,5 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect{|item, index| index != 1 ? item + 's' : x} 
+  array.each_with_index.collect{|item, index| index != 1 ? item + 's' : item}
 end
